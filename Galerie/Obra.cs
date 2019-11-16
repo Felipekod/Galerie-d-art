d4fc72b7cdf8ed820a33e94eaf4ef1609267911e
Felipe Oliveira <felipe.kod@gmail.com>
2019-11-16 07:19:46 -0500

    Projeto Galeria v 1.0

---
+++ Galerie/Obra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galerie
+{
+    public class Obra
+    {
+        public int code;
+        public string titre;
+        public string artiste;
+        public double valeur;
+        public double PrixVente = 0.00;
+    }
+
+}
